9775666f7a80175995b094457d88d08b5de98501
thedoomed <46077863+thedoomed@users.noreply.github.com>
2021-06-24 15:09:03 -0400

    Update Example1.cpp

--- Examples/Example1.cpp
+++ Examples/Example1.cpp
@@ -1,11 +1,13 @@
 #include <Windows.h>
 #include <iostream>
-#include "disasm.hpp"
+#include "DISA/disa.hpp"
 
 void my_function()
 {
 	printf("Example...\n");
-	printf("DISA is hot\n");
+	printf("DISA is good\n");
+	int i = 3;
+	printf("%i\n", i);
 }
 
 int main()
@@ -14,7 +16,7 @@ int main()
 	disa_load();
 
 	// Read the first 8 instructions in our function `my_function`
-	for (const auto& i : disa_read(reinterpret_cast<std::uintptr_t>(&my_function), 8))
+	for (const auto& i : disa_read(reinterpret_cast<std::uintptr_t>(&my_function), 12))
 	{
 		std::cout << i.data << std::endl;
 	}
