055888524e0db5b1534a058a875cc7fe980213e0
thedoomed <46077863+thedoomed@users.noreply.github.com>
2021-06-25 12:59:17 -0400

    Update and rename disa_detours.hpp to disa_debug.hpp

--- DISA/Utility/disa_debug.hpp
+++ DISA/Utility/disa_debug.hpp
@@ -1,12 +1,12 @@
 #include "../disa.hpp"
 
-struct detour_results
+struct disa_debug_results
 {
 	std::vector<std::uintptr_t>reg_contents;
 	std::uintptr_t reg;
 };
 
-class disa_detour
+class disa_debug
 {
 private:
 	std::vector<std::uint8_t>old_bytes;
@@ -18,17 +18,19 @@ private:
 	std::uint32_t timeout;
 	std::uintptr_t current_hook;
 public:
-	disa_detour();
-	~disa_detour();
+	disa_debug();
+	disa_debug(std::uintptr_t);
+	~disa_debug();
 
 	std::uintptr_t address;
-	detour_results result; // applies only to the specified register
+	disa_debug_results result; // applies only to the specified register
 
+	void set_address(std::uintptr_t location);
 	void set_reg32(std::uint8_t reg32);
 	void set_reg_offset(std::uint32_t offset); // offset from the register to dump
 	void set_dump_size(std::size_t count); // total number of offsets to dump from the register
 	void set_hit_count(std::size_t count); // total number of times the hook can be used before returning (if suspend is set to true)
 	void set_timeout(std::uint32_t ms); // total number of times the hook can be used before returning (if suspend is set to true)
-	bool start(bool suspend = false);
+	bool start(bool suspend = true);
 	void stop();
 };
