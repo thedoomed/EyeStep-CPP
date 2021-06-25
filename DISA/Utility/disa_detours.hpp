97e56e84ea63d15f14831d94894c5c05fed96e74
thedoomed <46077863+thedoomed@users.noreply.github.com>
2021-06-25 12:45:23 -0400

    Update disa_detours.hpp

--- DISA/Utility/disa_detours.hpp
+++ DISA/Utility/disa_detours.hpp
@@ -15,6 +15,7 @@ private:
 	std::size_t maxhits;
 	std::uint8_t debug_reg32;
 	std::uint32_t reg_offset;
+	std::uint32_t timeout;
 	std::uintptr_t current_hook;
 public:
 	disa_detour();
@@ -27,6 +28,7 @@ public:
 	void set_reg_offset(std::uint32_t offset); // offset from the register to dump
 	void set_dump_size(std::size_t count); // total number of offsets to dump from the register
 	void set_hit_count(std::size_t count); // total number of times the hook can be used before returning (if suspend is set to true)
+	void set_timeout(std::uint32_t ms); // total number of times the hook can be used before returning (if suspend is set to true)
 	bool start(bool suspend = false);
 	void stop();
 };
